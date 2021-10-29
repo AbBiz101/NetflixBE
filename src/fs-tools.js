8bdd8c0b5293ba6831e959146dcc6d6e21f853cf
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-29 14:43:57 +0200

    Rounter done

--- src/fs-tools.js
+++ src/fs-tools.js
@@ -3,7 +3,13 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
 const { readJSON, writeJSON, writeFile, createReadStream } = fs;
-const mediaJSON = join(
-	dirname(fileURLToPath(import.meta.url)),
-	'../src/data/media.json',
-);
+const dataJSON = join(dirname(fileURLToPath(import.meta.url)), '../src/DB');
+
+const mediaJSON = join(dataJSON, 'media.json');
+const reviewsJSON = join(dataJSON, 'reviews.json');
+
+export const getMedia = () => readJSON(mediaJSON);
+export const getReviews = () => readJSON(reviewsJSON);
+
+export const writeMedia = (media) => writeJSON(mediaJSON, media);
+export const writeReviews = (review) => writeJSON(reviewsJSON, review);
--- src/server/netflix.js
+++ src/server/netflix.js
@@ -2,10 +2,11 @@ import multer from 'multer';
 import uniqid from 'uniqid';
 import express from 'express';
 import createHttpError from 'http-errors';
-import { mediaValidator } from '../server/validator.js';
-import { validationResult, reviewValidator } from 'express-validator';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import { v2 as cloudinary } from 'cloudinary';
+import { validationResult } from 'express-validator';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { mediaValidator, reviewValidator } from '../server/validator.js';
+import { getMedia, getReviews, writeMedia, writeReviews } from '../fs-tools.js';
 
 const cloudinaryStorage = new CloudinaryStorage({
 	cloudinary,
@@ -15,6 +16,7 @@ const cloudinaryStorage = new CloudinaryStorage({
 });
 
 const netflixRounter = express.Router();
+//......................post ....media.....poster......review.............
 
 netflixRounter.post('/', mediaValidator, async (req, res, next) => {
 	try {
@@ -22,37 +24,59 @@ netflixRounter.post('/', mediaValidator, async (req, res, next) => {
 		if (!errorsList.isEmpty()) {
 			next(createHttpError(400, { errorsList }));
 		} else {
-			const author = await getAuthor();
-			const newAuthor = {
+			const allMedias = await getMedia();
+			const newMedia = {
 				...req.body,
-				createdAt: new Date(),
-				id: uniqid(),
+				imdbID: uniqid(),
 			};
-			author.push(newAuthor);
-			await writeAuthor(author);
-			res.status(201).send('Post created');
+			allMedias.push(newMedia);
+			await writeMedia(allMedias);
+			res.status(201).send('Media is successfully added to the database');
 		}
 	} catch (error) {
 		next(error);
 	}
 });
 
-netflixRounter.post('/:id', async (req, res, next) => {
+netflixRounter.post(
+	'/:id/poster',
+	multer({ storage: cloudinaryStorage }).single('moviePoster'),
+	async (req, res, next) => {
+		try {
+			const allMedias = await getMedia();
+			const mediaIndex = allMedias.findIndex((p) => p.imdbID === req.params.id);
+			const editedMedia = { ...allMedias[mediaIndex], Poster: req.file.path };
+			allMedias[mediaIndex] = editedMedia;
+			await writeMedia(allMedias);
+			res.send(editedMedia);
+		} catch (error) {
+			next(error);
+		}
+	},
+);
+
+netflixRounter.post('/:id/review', reviewValidator, async (req, res, next) => {
 	try {
+		const allReview = await getReviews();
+		const newReview = {
+			...req.body,
+			elementId: req.params.id,
+			_id: uniqid(),
+		};
+		allReview.push(newReview);
+		await writeReviews(allReview);
+		res.status(201).send('Review is successfully added to the database');
 	} catch (error) {
 		next(error);
 	}
 });
 
-netflixRounter.post('/:id', reviewValidator, async (req, res, next) => {
-	try {
-	} catch (error) {
-		next(error);
-	}
-});
+//......................get ....media..........review.............
 
 netflixRounter.get('/', async (req, res, next) => {
 	try {
+		const allMedias = await getMedia();
+		res.send(allMedias);
 	} catch (error) {
 		next(error);
 	}
@@ -60,27 +84,83 @@ netflixRounter.get('/', async (req, res, next) => {
 
 netflixRounter.get('/:id', async (req, res, next) => {
 	try {
+		const allMedias = await getMedia();
+		const singleMedias = allMedias.filter((p) => p.imdbID === req.params.id);
+		res.send(singleMedias);
 	} catch (error) {
 		next(error);
 	}
 });
 
-netflixRounter.put('/:id', async (req, res, next) => {
+netflixRounter.get('/:id/review', async (req, res, next) => {
 	try {
+		const allReview = await getReviews();
+		const singleReview = allReview.filter((p) => p.elementId === req.params.id);
+		res.send(singleReview);
 	} catch (error) {
 		next(error);
 	}
 });
 
+//......................put ....media.....poster......review.............
+
 netflixRounter.put('/:id', async (req, res, next) => {
 	try {
+		const allMedias = await getMedia();
+		const mediaIndex = allMedias.findIndex((p) => p.imdbID !== req.params.id);
+		const updatedMedia = { ...allMedias[mediaIndex], ...req.body };
+		allMedias[mediaIndex] = updatedMedia;
+		await writeMedia(allMedias);
+		res.send(updatedMedia);
 	} catch (error) {
 		next(error);
 	}
 });
+
+netflixRounter.put('/:id/review', async (req, res, next) => {
+	try {
+		const allMedias = await getMedia();
+	} catch (error) {
+		next(error);
+	}
+});
+
+netflixRounter.put(
+	'/:id/poster',
+	multer({ storage: cloudinaryStorage }).single('moviePoster'),
+	async (req, res, next) => {
+		try {
+			const allMedias = await getMedia();
+			const mediaIndex = allMedias.findIndex((p) => p.imdbID === req.params.id);
+			const editedMedia = { ...allMedias[mediaIndex], Poster: req.file.path };
+			allMedias[mediaIndex] = editedMedia;
+			await writeMedia(allMedias);
+			res.send(editedMedia);
+		} catch (error) {
+			next(error);
+		}
+	},
+);
+
+//......................delete ....media.....poster......review.............
 
 netflixRounter.delete('/:id', async (req, res, next) => {
 	try {
+		const allMedias = await getMedia();
+		const singleMediaOut = allMedias.filter((p) => p.imdbID !== req.params.id);
+		await writeMedia(singleMediaOut);
+		res.status(204).send();
+	} catch (error) {
+		next(error);
+	}
+});
+
+netflixRounter.delete('/:id/review', async (req, res, next) => {
+	try {
+		const allReview = await getReviews();
+		const oneReviewOut = allReview.filter((p) => p._id !== req.params.id);
+		await writeReviews(oneReviewOut);
+		res.status(204).send();
 	} catch (error) {
 		next(error);
 	}
