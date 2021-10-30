84c1a3e3f4148acefd4809e74c2ebca0c37cc079
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-30 17:47:46 +0200

    lol

--- src/server/validator.js
+++ src/server/validator.js
@@ -3,7 +3,7 @@ export const mediaValidator = [
 	body('Title').exists().withMessage('Title of the movie is mandatory field.'),
 	body('Type').exists().withMessage('Type is mandatory field.'),
 	body('Year').exists().withMessage('Year is mandatory field.'),
-	body('Poster').exists().withMessage('avatar is mandatory field.'),
+	// body('Poster').exists().withMessage('avatar is mandatory field.'),
 ];
 
 export const reviewValidator = [
